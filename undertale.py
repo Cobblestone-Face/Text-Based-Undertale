e00052bc83ae4e305019d770be8b7c69094422eb
Cobblestone-Face <78454545+Cobblestone-Face@users.noreply.github.com>
2022-10-27 13:42:24 -0500

    Update undertale.py
    updated with some comments

--- undertale.py
+++ undertale.py
@@ -5,8 +5,10 @@ Description:
 undertale battle mechanic thing idk
 """
 import random
+import sys
 
 def generateBattle(opponent, hp, opHp, image):
+    print() # clears the screen
     print()
     print()
     print()
@@ -97,51 +99,51 @@ def generateBattle(opponent, hp, opHp, image):
     print()
     print()
     print()
+    print(opponent) # shows your opponent's name
     print()
-    print(opponent)
+    print("HP:") 
+    print("🟨" * opHp) # displays opponent hp in a visually pleasing way
     print()
-    print("HP:")
-    print("🟨" * opHp)
-    print()
-    print("      " + str(image))
+    print("      " + str(image)) # emoji for opponent
     print("______________")
-    print("|            |")
+    print("|            |") # udnetrale
     print("|            |")
     print("|            |")
     print("|     ❤️     |")
     print("|            |")
     print("|            |")
     print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-    print("HP: " + str(hp))
+    print("HP: " + str(hp)) # displays user hp
     print()
 
 def options(opponentHealth):
-    choice = input("|FIGHT (f)|  |ACT (a)|  |MERCY (m)|")
-    if choice == "f":
+    choice = input("|FIGHT (f)|  |ACT (a)|  |MERCY (m)| ") # options
+    if choice == "f": # fight
         attack = random.randint(1,10)
         num = input("Pick a number between 1 and 10. ")
         damage = 10 - abs(attack - int(num))
         print("The number was " + str(attack) + ", you picked " + str(num) + ".")
         input("You did " + str(damage) + " damage. (enter to continue) ")
         return damage
-    if choice == "a":
+    if choice == "a": # act
         print("Temporarily unavailable :(")
-    if choice == "m":
-        mercy = input("Spare (s) or Flee (f)?")
+        sys.exit()
+    if choice == "m": # mercy
+        mercy = input("Spare (s) or Flee (f)? ")
         if mercy == "f":
             print("You fled safely")
         if mercy == "s":
-            print("you got a strike instead")
+            print("you got a strike instead") # bowling joke lmao
         end = "y"
         return end
 def opponentAtk(hp, difficulty):
     print()
-    print("(This dodge system is temporary until i can think of something better :P)")
+    print("(This dodge system is temporary until i can think of something better :P)") # because im stupid
     print("The attack is level " + str(difficulty) + " out of 5.")
-    dodge = random.randint(1,10)
-    luckIGuess = input("Pick a number between 1 and 10. ")
-    damageTaken = abs(dodge - int(luckIGuess)) - difficulty
+    dodge = random.randint(1,10) # generates a random number the player needs to guess
+    luckIGuess = input("Pick a number between 1 and 10. ") # player guesses the number
+    damageTaken = abs(dodge - int(luckIGuess)) - difficulty # math :(
     print("The number was " + str(dodge) + ", you chose " + str(luckIGuess) + ", and the damage was increased by " + str(difficulty) + " from the difficulty level.")
     input("You took " + str(damageTaken) + " damage. (enter to continue) ")
-    return damageTaken
+    return damageTaken # returns how much damage you took to calculate in the main program
     
